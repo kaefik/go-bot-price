8d817dcf889ac0543eaf92ca37aca719e85d5b0a
Ilnur Saifutdinov <ilnursoft@gmail.com>
2015-11-22 11:59:19 +0300

    Many change into main file

--- go-bot-labirint.go
+++ go-bot-labirint.go
@@ -23,5 +23,9 @@ func gethtmlfromurl(suri string) (string,error) {
 
 func main() {
 	fmt.Println("Hello World!")
-	gethtmlfromurl()
+	//surl:="http://labirint.ru"
+	surl:="https://git-scm.com/book/ru/v1/%D0%9E%D1%81%D0%BD%D0%BE%D0%B2%D1%8B-Git-%D0%97%D0%B0%D0%BF%D0%B8%D1%81%D1%8C-%D0%B8%D0%B7%D0%BC%D0%B5%D0%BD%D0%B5%D0%BD%D0%B8%D0%B9-%D0%B2-%D1%80%D0%B5%D0%BF%D0%BE%D0%B7%D0%B8%D1%82%D0%BE%D1%80%D0%B8%D0%B9"
+	shtml,serr:=gethtmlfromurl(surl)
+	fmt.Println(shtml)
+	fmt.Println(serr)
 }
